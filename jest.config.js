4bf01106fd63d357aee9e63070b25a020d9b4511
Giordan Arredondo <garredondop@uni.pe>
2024-04-06 12:18:29 -0500

    Configuration files

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)','**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    transform: {
+      '^.+\\.ts?$': 'ts-jest',
+    },
+    moduleNameMapper: {
+      '@/(.*)': '<rootDir>/src/$1',
+    },
+  };
+  
